9c24e83268fcbd170f20a2478c56330072d61078
sonyx <elizalde50@gmail.com>
2015-07-08 15:54:53 -0500

    Examen

--- TestLatam/Controllers/HomeController.cs
+++ TestLatam/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace TestLatam.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             ViewBag.Title = "Home Page";
 
